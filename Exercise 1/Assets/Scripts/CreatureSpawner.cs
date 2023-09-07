94cb4f73b055c11900e83b04e91e7ea0e9b7720d
Jason <c04jason@gmail.com>
2023-09-07 11:50:07 -0400

    vector2

--- Exercise 1/Assets/Scripts/CreatureSpawner.cs
+++ Exercise 1/Assets/Scripts/CreatureSpawner.cs
@@ -1,4 +1,4 @@
-using system;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +7,9 @@ public class CreatureSpawner : MonoBehaviour
 {
     [SerializeField]
     GameObject creaturePrefab;
+
+    [SerializeField]
+    Vector2 spawnLocation;
 
     // Start is called before the first frame update
     void Start()
