439d6698d73ec2f8eb041665c4c9adb9b9434c8e
Jason <c04jason@gmail.com>
2023-09-07 15:13:05 -0400

    Spawns 3 Araxenty

--- Exercise 1/Assets/Scripts/CreatureSpawner.cs
+++ Exercise 1/Assets/Scripts/CreatureSpawner.cs
@@ -12,12 +12,24 @@ public class CreatureSpawner : MonoBehaviour
     string creatureName = "Araxenty";
 
     [SerializeField]
-    Vector3 spawnLocation;
+    List<Vector3> spawnLocation;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initialize spawnLocation and add 3 locations
+        spawnLocation = new List<Vector3>(3)
+        {
+            new Vector3(0, 0, 10),
+            new Vector3(0, 0, 20),
+            new Vector3(0, 0, 30)
+        };
+
+        // Instantiate each prefab
+        foreach (var cord in spawnLocation)
+        {
+            Instantiate(creaturePrefab, new Vector3(cord.x, cord.y, cord.z), Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
