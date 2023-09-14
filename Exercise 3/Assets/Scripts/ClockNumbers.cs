a801faefc98b3f0477d2ea8defa19f4bb415b0df
Jason <c04jason@gmail.com>
2023-09-14 15:13:58 -0400

    started clock numbers

--- Exercise 3/Assets/Scripts/ClockNumbers.cs
+++ Exercise 3/Assets/Scripts/ClockNumbers.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ClockNumbers : MonoBehaviour
 {
+    [SerializeField]
+    TextMesh clockNumber;
+
     // Start is called before the first frame update
     void Start()
     {
