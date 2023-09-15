b5c902aaf9b254c01978d59f820a5d450ca18878
Jason <c04jason@gmail.com>
2023-09-14 22:19:19 -0400

    clock number script

--- Exercise 3/Assets/Scripts/ClockNumbers.cs
+++ Exercise 3/Assets/Scripts/ClockNumbers.cs
@@ -7,15 +7,26 @@ public class ClockNumbers : MonoBehaviour
     [SerializeField]
     TextMesh clockNumber;
 
+    [SerializeField]
+    List<TextMesh> clock;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        clock = new List<TextMesh>(12)
+        {
+            clockNumber
+        };
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float degrees = 0f;
+        for (int i = 3; i < 15; i++)
+        {
+            Instantiate(clockNumber, new Vector3(0f, degrees, 0f), Quaternion.identity);
+            degrees += 30;
+        }
     }
 }
