484476c3cf2c5feed70380c5d92ba507e9f00ad1
Jason <c04jason@gmail.com>
2023-09-21 14:04:10 -0400

    edit

--- Exercise 4/Assets/Scripts/InputController.cs
+++ Exercise 4/Assets/Scripts/InputController.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class InputController : MonoBehaviour
 {
-    MovementController controller = new MovementController();
-
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +18,6 @@ public class InputController : MonoBehaviour
 
     public void OnMove()
     {
-        // Get the latest value for the input from the Input System
-        inputDirection = context.ReadValue<Vector2>();  // This is already normalized for us
-
-        // Send that new direction to the Vehicle class
-        myMovmentController.SetDirection(inputDirection);
 
     }
 }
